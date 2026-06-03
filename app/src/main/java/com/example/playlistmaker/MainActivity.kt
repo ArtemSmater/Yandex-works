57c6d65719aed31283bda5afee24b72335698221
Artem <twistersister987@gmail.com>
2026-06-03 12:58:13 +0300

    correct last commit

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,14 +1,57 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val searchButton = findViewById<Button>(R.id.btnSearch)
+        val mediaButton = findViewById<Button>(R.id.btnMedia)
+        val settingsButton = findViewById<Button>(R.id.btnSettings)
+
+        searchButton.setOnClickListener(this)
+        mediaButton.setOnClickListener(listener1)
+        settingsButton.setOnClickListener(listener2)
+    }
+
+    // var 1
+    override fun onClick(v: View?) {
+        checkButton(v as Button)
+    }
+
+    // var 2
+    val listener1: View.OnClickListener = View.OnClickListener { v -> checkButton(v as Button) }
+
+    // var 3
+    val listener2: View.OnClickListener = object : View.OnClickListener {
+        override fun onClick(v: View?) {
+            checkButton(v as Button)
+        }
+    }
+
+    fun checkButton(btn: Button) {
+        val intent = when (btn.id) {
+            R.id.btnSearch -> Intent(this, SearchActivity::class.java)
+            R.id.btnMedia -> Intent(this, MediaActivity::class.java)
+            R.id.btnSettings -> Intent(this, SettingsActivity::class.java)
+            else -> Intent(this, MediaActivity::class.java)
+        }
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -0,0 +1,122 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SearchActivity : AppCompatActivity() {
+
+    companion object {
+        const val DEF_LINE = ""
+        const val DEF_CURSOR = 0
+    }
+
+    private lateinit var toolbar: Toolbar
+    private lateinit var ivClear: ImageView
+    private lateinit var etSearch: EditText
+    private var searchValue = DEF_LINE
+    private var searchCursor = DEF_CURSOR
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+        initViews()
+
+        if (savedInstanceState != null) {
+            searchValue = savedInstanceState.getString(resources.getString(R.string.save_state_key), DEF_LINE)
+            searchCursor = savedInstanceState.getInt(resources.getString(R.string.save_cursor_key), DEF_CURSOR)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.search)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        if (!searchValue.isEmpty()) {
+            outState.putString(resources.getString(R.string.save_state_key), searchValue)
+            outState.putInt(resources.getString(R.string.save_cursor_key), etSearch.selectionStart)
+        }
+    }
+
+    fun initViews() {
+        toolbar = findViewById(R.id.tbSearch)
+        ivClear = findViewById(R.id.ivClear)
+        etSearch = findViewById(R.id.etSearch)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        listeners()
+        setText(searchValue, searchCursor)
+    }
+
+    fun listeners() {
+        ivClear.setOnClickListener { _ ->
+            run {
+                setText(DEF_LINE, DEF_CURSOR)
+                val inputMethodManager =
+                    getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
+                inputMethodManager?.hideSoftInputFromWindow(etSearch.windowToken, 0)
+            }
+        }
+        toolbar.setNavigationOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    MainActivity::class.java
+                )
+            )
+        }
+
+        etSearch.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+
+            }
+
+            override fun onTextChanged(
+                s: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                searchValue = s.toString().trim()
+                hideClearButton()
+            }
+        })
+    }
+
+    fun hideClearButton() {
+        if (etSearch.text.trim().isEmpty()) {
+            ivClear.visibility = View.GONE
+        } else {
+            ivClear.visibility = View.VISIBLE
+        }
+    }
+
+    fun setText(line: String, selection: Int) {
+        etSearch.setText(line)
+        etSearch.setSelection(selection)
+    }
+}
